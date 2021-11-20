8a56f3af8bbc9d2a91f20f2e226ee66f8053525b
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2021-11-20 12:43:20 +0200

    Stack with Dynamic Array
    Fully implemented a stack with Dynamic Array.
    Still needs documentation.

--- Data Structures/DStack.h
+++ Data Structures/DStack.h
@@ -1,21 +1,36 @@
+/*-- DStack.h --------------------------------------------------------------
+
+ This header file defines a Stack data type.
+ Basic operations:
+ constructor:  Constructs an empty stack
+ empty:        Checks if a stack is empty
+ push:         Modifies a stack by adding a value at the top
+ top:          Accesses the top stack value; leaves stack unchanged
+ pop:          Modifies stack by removing the value at the top
+ display:      Displays all the stack elements
+ Note: Execution terminates if memory isn't available for a stack element.
+ --------------------------------------------------------------------------*/
 #include <iostream>
 using namespace std;
 typedef int StackElement;
 
-class DStack {
+class DStack 
+{
+private:
+	/***** Data Members *****/
+	int myCapacity;
+	int myTop;
+	StackElement* myArray;
 public:
+	/***** Function Members *****/
+	/***** Constructors *****/
 	DStack(int numElements = 128);
 	DStack(const DStack& original);
 	const DStack& operator= (const DStack& righthandside);
+	~DStack();
 	bool empty() const;
 	void push(const StackElement& value);
-	~DStack();
 	void display(ostream& out) const;
 	StackElement top() const;
 	void pop();
-private:
-	int mycapacity;
-	int mytop;
-	StackElement* myarray;
-
 };
